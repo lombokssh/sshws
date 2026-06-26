5979271a446a3b60eac640784f36e375fba9b6ae
Zulzario Zaeri <ytryo789@gmail.com>
2026-06-27 07:21:40 +0800

    feat: change name

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ apt install build-essential -y
 
 # Download binary release
 # Ambil versi rilis terbaru dari GitHub API
-LATEST_VERSION=$(curl -s https://api.github.com/repos/ryotwell/engsel.sshws/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)
+LATEST_VERSION=$(curl -s https://api.github.com/repos/lombokssh/sshws/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)
 echo "Mengunduh versi terbaru: $LATEST_VERSION"
 
-wget -O /usr/local/bin/engsel-sshws "https://github.com/ryotwell/engsel.sshws/releases/download/${LATEST_VERSION}/engsel-sshws"
-chmod +x /usr/local/bin/engsel-sshws
+wget -O /usr/local/bin/sshws "https://github.com/lombokssh/sshws/releases/download/${LATEST_VERSION}/sshws"
+chmod +x /usr/local/bin/sshws
 
 # Buat file service systemd
-cat > /etc/systemd/system/engsel-sshws.service <<EOF
+cat > /etc/systemd/system/sshws.service <<EOF
 [Unit]
 Description=SSH WS TLS Proxy
 Documentation=https://google.com
@@ -20,7 +20,7 @@ After=syslog.target network-online.target
 [Service]
 User=root
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/engsel-sshws
+ExecStart=/usr/local/bin/sshws
 Restart=on-failure
 RestartPreventExitStatus=23
 LimitNPROC=10000
@@ -31,5 +31,5 @@ WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-systemctl enable engsel-sshws.service
-systemctl restart engsel-sshws.service
+systemctl enable sshws.service
+systemctl restart sshws.service
